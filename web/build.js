4baaa4c9a57e1f774c829889d0f65cc7292c0b2a
mmsyst <miki.masayuki.mh1@is.naist.jp>
2023-10-25 13:19:50 +0900

    update

--- web/build.js
+++ web/build.js
@@ -1,8 +1,8 @@
-const esbuild = require("esbuild");
-const fs = require("fs");
-const path = require("path");
-const copy = require("esbuild-plugin-copy");
-const crypto = require("crypto");
+import { build } from "esbuild";
+import { existsSync, rmSync, readFileSync, writeFileSync } from "fs";
+import { basename, extname, join } from "path";
+import copy from "esbuild-plugin-copy";
+import { createHash } from "crypto";
 
 const outputDir = "./view/assets/js";
 // 各Typescriptをトランスパイルのエントリーポイントとして定義
@@ -10,13 +10,12 @@ const filePairs = [
   {ts: "ts/index.ts", html: "index.html"},
 ];
 // 出力ディレクトリが存在する場合、削除する
-if (fs.existsSync(outputDir)) {
-    fs.rmSync(outputDir, { recursive: true });
+if (existsSync(outputDir)) {
+    rmSync(outputDir, { recursive: true });
 }
 // Javascriptのファイル名にハッシュ値を使ってランダム要素を加える
 function generateHash(content) {
-    return crypto
-        .createHash("sha256")
+    return createHash("sha256")
         .update(content)
         .digest("hex")
         .slice(0, 8);
@@ -26,19 +25,19 @@ let isFirstPair = true;
 // ビルド
 for (const pair of filePairs) {
     // ファイルを読み込む
-    const fileContent = fs.readFileSync(pair.ts, 'utf-8');
+    const fileContent = readFileSync(pair.ts, 'utf-8');
     // ファイル名用ハッシュ値を生成
     const hash = generateHash(fileContent);
     // エントリーポイントのファイル名を取得
-    const baseName = path.basename(pair.ts, path.extname(pair.ts));
+    const baseName = basename(pair.ts, extname(pair.ts));
     // ビルド後のファイル名を定義
-    const outFile = path.join(outputDir, `${baseName}.${hash}.js`);
+    const outFile = join(outputDir, `${baseName}.${hash}.js`);
     // １回だけ実行するコピー処理
     const plugins = [];
     if (isFirstPair) {
         // 2回目以降はコピー処理を行わない
         plugins.push(
-            copy.default({
+            copy({
                 resolveFrom: "cwd",
                 assets: [
                     // この.cssファイルは、Bootstrap Studioの各印刷用のページのProperties->HeadContentでのみ設定済みのもの
@@ -59,7 +58,7 @@ for (const pair of filePairs) {
         console.log("--- copy files setting. ---");
     }
     // ビルド
-    esbuild.build({
+    build({
         entryPoints: [pair.ts],
         bundle: true,
         minify: true,
@@ -71,13 +70,13 @@ for (const pair of filePairs) {
         plugins,
     }).then(() => {
         // ビルド後にHTMLを更新
-        const htmlContent = fs.readFileSync(`html/${pair.html}`, 'utf-8');
+        const htmlContent = readFileSync(`html/${pair.html}`, 'utf-8');
         // ファイル名をHTMLに埋め込む
         const scriptPlaceholder = "%BUNDLE_JS_PATH%";
         // 整形後のHTMLファイル
         const updatedHtmlContent = htmlContent.replace(scriptPlaceholder, outFile);
         // 書き出し
-        fs.writeFileSync(`view/${pair.html}`, updatedHtmlContent);
+        writeFileSync(`view/${pair.html}`, updatedHtmlContent);
         console.log(`${pair.ts} -> ${outFile}`);
     }).catch((err) => console.error(err));
 }
